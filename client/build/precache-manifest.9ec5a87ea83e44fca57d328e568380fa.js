1337b77d90c756dc1c9b5c90a54580793f8a5590
JQueen <queen_jenf@yahoo.com>
2020-12-18 08:57:26 -0800

    final updates for build

---
+++ client/build/precache-manifest.9ec5a87ea83e44fca57d328e568380fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e9055d9f2c519ae09ae1d4c4eb4b3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95771947aa1e4bda4219",
+    "url": "/static/css/main.d1506c43.chunk.css"
+  },
+  {
+    "revision": "080f95299f895c69b4ae",
+    "url": "/static/js/2.e98da745.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.e98da745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95771947aa1e4bda4219",
+    "url": "/static/js/main.738aea1d.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
